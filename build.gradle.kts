fab6f66038e9799c50f5c1b01f6dd38ebe450bce
Patrick Schwager <patrick.schwager@telekom.de>
2025-06-16 17:49:00 +0200

    Use sonatype central

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,7 +50,7 @@ subprojects {
     }
 
     mavenPublishing {
-        publishToMavenCentral(SonatypeHost.DEFAULT, automaticRelease = true)
+        publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = true)
         signAllPublications()
 
         pom {
